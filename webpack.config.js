0d9ea40c542ee459d68e0318c6f9b3ed27f884b9
Oleh Zelinskyi <blackbard27@yahoo.com>
2020-07-03 12:11:07 +0300

    Installing dependencies. Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWepackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.tsx"),
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    compress: false,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWepackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
